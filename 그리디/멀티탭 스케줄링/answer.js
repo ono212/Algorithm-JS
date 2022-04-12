aa995efb66a20c69d0b6ba5197f8cfcb1138fa11
jungmin <qkrwjdals302@naver.com>
2022-04-12 20:11:59 +0900

    풀이 수정 및 예제 추가

--- 그리디/멀티탭 스케줄링/answer.js
+++ 그리디/멀티탭 스케줄링/answer.js
@@ -7,19 +7,21 @@
 */
 
 /*
-각 용품의 사용 횟수를 세서 count객체에 저장한다.
-사용할 때마다 -1을 해준다.
-
 ✅경우의 수
-1. 이미 꽂혀있는 경우  =>  -1
-2. 멀티탭 자리가 비어있을 경우  =>  -1
-3. 멀티탭이 꽉 차 있을 경우  =>  count로 뒤에서 사용되는지 여부 판단 후에 꽂는다.
+1. 이미 꽂혀있는 경우
+2. 멀티탭 자리가 비어있을 경우
+3. 멀티탭이 꽉 차 있을 경우
+  => ⭐남은 order를 확인하면서 현재 멀티탭에 꽂혀 있는 아이템들 중 가장 늦게 나오는 아이템을 뽑아야한다.
+  3-1. lastItem에 현재 multitab에 꽂혀있는 아이템들을 복사한다.
+  3-2. 남은 order를 순회하면서 multitab에 꽂혀있는 아이템일 경우, lastItem에서 삭제한다.
+  3-3. lastItem의 길이가 1이 되는 순간, lastItem에 있는 아이템이 가장 늦게 나오는 아이템이므로 뽑는다!
+
 */
 
 const path = require("path");
 
 let [line1, line2] = require("fs")
-  .readFileSync(path.resolve(__dirname, "./input.txt"))
+  .readFileSync(path.resolve(__dirname, "./input2.txt"))
   .toString()
   .trim()
   .split("\n");
@@ -29,38 +31,29 @@ const order = line2.split(" ").map(Number); // 사용 순서
 
 const multitab = [];
 let answer = 0;
-let count = {};
 
-for (let item of order) {
-  if (count[item]) {
-    count[item]++;
-  } else {
-    count[item] = 1;
-  }
-}
+for (let i = 0; i < K; i++) {
+  if (multitab.includes(order[i])) continue; // 이미 멀티탭에 꽂혀있는 경우
+  if (multitab.length < N) multitab.push(order[i]); // 멀티탭이 비어있을 경우
+  else {
+    /*
+    멀티탭이 꽉 차 있을 경우
+    1. N개 중에 뺄 것이 있는 경우 (멀티탭에서 N개에 포함되지 않는게 있을 경우)
+    2. N개 중에 뺄 것이 없는 경우 (이후 order 돌면서 가장 늦게 나오는 것을 빼야함)
+    */
 
-for (let i = 0; i < order.length; i++) {
-  if (multitab.includes(order[i])) {
-    count[order[i]]--;
-    continue;
-  }
-  if (multitab.length < N) {
-    // 멀티탭이 비어있을 경우
-    count[order[i]]--;
-    multitab.push(order[i]);
-  } else {
-    // 멀티탭이 꽉 차 있을 경우
-    let popItemIndex = 0;
-    let min = count[multitab[0]];
+    // 현재 멀티탭에 꽂혀 있는 물건들 중에서 가장 늦게 나오는 아이템을 담는 배열
+    // 현재 멀티탭에 꽂혀 있는 물건들을 slice로 복사한다.
+    let lastItem = multitab.slice();
 
-    for (let j = 1; j < multitab.length; j++) {
-      if (count[multitab[j]] < min) {
-        popItemIndex = j;
-        min = count[multitab[j]];
+    for (let j = i + 1; j < K; j++) {
+      if (lastItem.length === 1) break;
+      if (lastItem.includes(order[j])) {
+        lastItem.splice(lastItem.indexOf(order[j]), 1);
       }
     }
 
-    multitab.splice(popItemIndex, 1);
+    multitab.splice(multitab.indexOf(lastItem[0]), 1);
     multitab.push(order[i]);
 
     answer++;
