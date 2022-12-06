cacbbec0e220adfa99c763dfcd68233af7ae03dd
jungmin <qkrwjdals302@naver.com>
2022-12-07 03:45:44 +0900

    설명 추가

--- 다이나믹 프로그래밍/퇴사2/answer.js
+++ 다이나믹 프로그래밍/퇴사2/answer.js
@@ -6,7 +6,7 @@
 N이 최대 150만이므로 완전탐색 방식으로 풀 경우 시간초과가 발생한다. 
 
 상담을 시작하거나, 상담을 시작하지 않거나. => 2가지 경우
-dp[i] = i일동안 누적한 최대 이익
+dp[i] = i일까지 누적된 최대 이익
 
 [참조]
 https://cider.tistory.com/14
@@ -24,7 +24,7 @@ let [N, ...schedule] = require("fs")
 N = +N;
 
 schedule.map((e, idx, arr) => (arr[idx] = e.split(" ").map(Number)));
-
+console.log(schedule);
 const dp = new Array(N + 1).fill(0);
 let maximumProfit = 0;
 
@@ -35,6 +35,9 @@ for (let i = 0; i < N; i++) {
   if (i + time <= N) {
     dp[i + time] = Math.max(dp[i + time], maximumProfit + pay);
     // Math.max(오늘 시작하는 상담이 끝난 날의 수익, 현재까지의 최대 수익에 이번 상담의 수익을 더한 값)
+    // dp[i + time]은 i일 때 상담을 시작하면 상담이 끝나는 날이다. 즉, 상담이 끝나는 날의 값을 갱신해주고 있다.
   }
 }
 console.log(Math.max(...dp));
+
+// 상담을 못하는 상황일 수도 잇잖아 (이미 상담이 진행중이어서)
